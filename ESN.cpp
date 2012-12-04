529970a6e2df6a0cd18c561992eb94ab450de513
JImmy <jimmy@jimmy-VirtualBox.(none)>
2012-12-04 10:03:42 +0100

    New version of the ESN class

--- ESN.cpp
+++ ESN.cpp
@@ -3,18 +3,52 @@
  *                Supervisors: Ralf Der, Georg Martius                       *  
 
  * Members: Fabien Benureau, Chrisantha Fernando, Quan Wang, Jimmy Baraglia  *
- *                   Echo State Network Header File                          *
+ *                    Echo State Network c++ File                            *
  *                                                                           *
  *****************************************************************************/
 
 #include "ESN.h"
 
 using namespace std;
+using namespace matrix;
 
-/**
- * ESN class constructor
- */
-void ESN::ESN()
-{
+  /**
+   * ESN class constructor
+  */
+  void ESN::ESN()
+  {
 
-}
+  }
+
+  virtual void ESN::init(unsigned int inputDim, unsigned  int outputDim,double unit_map.0, RandGen* randGen)
+  {
+
+  }
+
+
+  virtual const Matrix ESN::process (const Matrix& input)
+  {
+
+  }
+
+  
+  virtual const Matrix ESN::learn (const Matrix& input, const Matrix& nom_output, double learnRateFactor = 1)
+  {
+
+  }
+
+  virtual void ESN::damp(double damping)
+  {
+
+  }
+
+  virtual unsigned int ESN::getInputDim() const
+  {
+
+  }
+
+  virtual unsigned int ESN::getOutputDim() const
+  {
+
+  }
+
--- ESN.h
+++ ESN.h
@@ -11,7 +11,9 @@
 
 
 #include <stdio.h>
-#include "abstractmodel.h"
+#include <selforg/abstractmodel.h>
+#include <selforg/matix.h>
+
 
 /**
  * class for robot control with sine, sawtooth and impuls  
@@ -21,7 +23,6 @@
  */
 class ESN : public AbstractModel {
 public:
-  enum function {Sine, SawTooth, Impulse};
 
   /**     
      @param controlmask bitmask to select channels to control (default all)
@@ -29,9 +30,47 @@ public:
    */
   ESN();
 
+  /** initialisation of the network with the given number of input and output units
+      @param inputDim length of input vector
+      @param outputDim length of output vector
+      @param unit_map if 0 the parametes are choosen randomly. 
+             Otherwise the model is initialised to represent a unit_map 
+	     with the given response strength.
+      @param randGen pointer to random generator, if 0 an new one is used
+   */
+  virtual void init(unsigned int inputDim, unsigned  int outputDim, 
+		    double unit_map.0, RandGen* randGen);
+
+  /** passive processing of the input
+     (this function is not constant since a recurrent network 
+     for example might change internal states
+  */
+  virtual const matrix::Matrix process (const matrix::Matrix& input);
+
+  /* performs learning and returns the network output before learning.
+     Neural networks process the input before. (no need to call process before)
+     \param learnRateFactor can be given to modify eps for this learning step.
+  */
+  virtual const matrix::Matrix learn (const matrix::Matrix& input, 
+				      const matrix::Matrix& nom_output, 
+				      double learnRateFactor = 1);
+
+  /// damps the weights and the biases by multiplying (1-damping)
+  virtual void damp(double damping);
+
+  /// returns the number of input neurons
+  virtual unsigned int getInputDim() const;
+  /// returns the number of output neurons
+  virtual unsigned int getOutputDim() const;
+
   
 
 protected:
+
+  matrix::Matrix inputWeights;
+  matrix::Matrix outputWeights;
+  matrix::Matrix ESNNeurons;
+  matrix::Matrix ESNWeights; 
 
   //
 };
