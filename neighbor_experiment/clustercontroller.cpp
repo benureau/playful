6dfdf8fbaf6975a8af5eb8375a600e22415b4f97
Fabien Benureau <fabien.benureau@inria.fr>
2012-12-05 17:13:09 +0100

    added clustercontroller, inherited from Sox

---
+++ neighbor_experiment/clustercontroller.cpp
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2011 by                                            *
+ *    Georg Martius  <georg dot martius at web dot de>                     *
+ *    Ralf Der       <ralfder at mis dot mpg dot de>                       *
+ *                                                                         *
+ *   ANY COMMERCIAL USE FORBIDDEN!                                         *
+ *   LICENSE:                                                              *
+ *   This work is licensed under the Creative Commons                      *
+ *   Attribution-NonCommercial-ShareAlike 2.5 License. To view a copy of   *
+ *   this license, visit http://creativecommons.org/licenses/by-nc-sa/2.5/ *
+ *   or send a letter to Creative Commons, 543 Howard Street, 5th Floor,   *
+ *   San Francisco, California, 94105, USA.                                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "clustercontroller.h"
+using namespace matrix;
+using namespace std;
+
+ClusterController::ClusterController(const SoxConf& conf)
+  : Sox(conf)
+{
+}
+
+
+ClusterController::ClusterController(double init_feedback_strength, bool useExtendedModel, bool useTeaching )
+  : Sox(init_feedback_strength, useExtendedModel, useTeaching)
+{
+}
+
+ClusterController::~ClusterController(){
+}
