81154838849c61d083f4cc32e6b703951a84ccc0
Fabien Benureau <fabien.benureau@inria.fr>
2012-12-05 17:13:39 +0100

    added kmeans sample code

---
+++ neighbor_experiment/kmeanscv.cpp
@@ -0,0 +1,41 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp"
+#include <iostream>
+ 
+using namespace cv;
+using namespace std;
+ 
+ 
+int main( int /*argc*/, char** /*argv*/ )
+{
+ cout << "\n Usage in C++ API:\n double kmeans(const Mat& samples, int clusterCount, Mat& labels, TermCriteria termcrit, int attempts, int flags, Mat* centers) \n\n\n" << endl;
+ 
+ int clusterCount = 2;
+ int dimensions = 5;
+ int sampleCount = 50;
+ 
+ 
+ Mat points(sampleCount,dimensions, CV_32F,Scalar(10));
+ Mat labels;
+ Mat centers(clusterCount, 1, points.type());
+ 
+ 
+ // values of 1st half of data set is set to 10
+ //change the values of 2nd half of the data set; i.e. set it to 20
+ 
+ for(int i =24;i<points.rows;i++)
+ {
+  for(int j=0;j<points.cols;j++)
+  {
+   points.at<float>(i,j)=20;
+  }
+ }
+ 
+ 
+ kmeans(points, clusterCount, labels, TermCriteria( CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 10, 1.0), 3, KMEANS_PP_CENTERS, centers);
+    // we can print the matrix directly.
+ cout<<"Data: \n"<<points<<endl;
+ cout<<"Center: \n"<<centers<<endl;
+ cout<<"Labels: \n"<<labels<<endl;
+ return 0;
+}
